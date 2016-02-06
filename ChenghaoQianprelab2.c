41a2e3df7b6f2c2ed85e11f88c124638e3ed9c7e
ChenghaoQ <ch.jakobqian@gmail.com>
2016-02-06 11:02:57 -0600

    New homework

---
+++ ChenghaoQianprelab2.c
@@ -0,0 +1,38 @@
+//Name: Chenghao Qian
+//Pawprint: cqnp3
+
+
+#include<stdio.h>
+int main(void)
+{
+	int first,second,sum,product;
+	
+	printf("Enter the first Number:\n");
+	scanf("%d",&first);
+
+	if(first<0) {
+		printf("No negative number please!\n");
+		printf("Enter a new value:\n");
+		scanf("%d",&first);
+	}
+
+	printf("Enter the second number:\n");
+	scanf("%d",&second);
+
+	if(second<0) {
+		printf("No negative number please!\n");
+		printf("Enter a new value:\n");
+		scanf("%d",&second);
+	}
+
+	sum = first + second;
+	product = first * second;
+
+	printf("\nADDITION");
+	printf("\nThe sum of %d and %d is %d",first,second,sum);
+
+	printf("\nMUTIPLICATION");
+	printf("\nThe prodcut of %d and %d is %d\n",first,second,product);
+
+	return 0;
+}
