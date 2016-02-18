2bc8281be6a34a3d1dfcacb3d50c7dcae3154d31
Zhongyu Li <zli@localhost.localdomain>
2016-02-18 06:39:55 -0600

    prelab4 for check Error and Even

---
+++ prelab4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int num,result;
+int main()
+{
+int checkError();
+int checkEven();
+//	int num,result;
+
+	printf("Enter a positive number:");
+	scanf("%d",&num);
+	checkError();
+	checkEven();
+	return 0;
+}
+int checkError()
+{
+	while (num < 0) {
+	printf("\nError!Enter a positive number only:");
+	scanf("%d",&num);
+	}
+	return 0; 
+}
+int checkEven()
+{
+	result=num%2;
+	if (result = 0) {
+		printf("\n%d is an Even number\n",num);
+	}
+	else	{
+		printf("\n%d is an Odd number\n",num);
+	return 0;
+}
+}
