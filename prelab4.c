dfa08ce44b0dbe26e4aa5e7f18cc9927c490ce0f
ChenghaoQ <ch.jakobqian@gmail.com>
2016-02-18 10:19:43 -0600

    new bug fixed

--- prelab4.c
+++ prelab4.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int num,result;
-int main()
-{
+
 int checkError();
 int checkEven();
+int main()
+{
 //	int num,result;
 
 	printf("Enter a positive number:");
@@ -18,16 +19,16 @@ int checkError()
 	printf("\nError!Enter a positive number only:");
 	scanf("%d",&num);
 	}
-	return 0; 
+	return 0 ; 
 }
 int checkEven()
 {
 	result=num%2;
-	if (result = 0) {
+	if (result == 0) {
 		printf("\n%d is an Even number\n",num);
 	}
 	else	{
 		printf("\n%d is an Odd number\n",num);
-	return 0;
+	return 0 ;
 }
 }
