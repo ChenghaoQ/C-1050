882edd19f76ab9f1f778daf6201bc8499d3af442
Chenghao <ch.jakobqian@gmail.com>
2016-04-24 21:43:25 -0500

    okokok

---
+++ fileprocess.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+
+int load_data(char*,int*,float*,int*)
+void print_data(int*,float *, int)
+int highest_amount(float *, int)
+int lowest_amount(float *,int)
+int average_amount(float *, int)
+int update_data(char*,int*,float*,int)
+void write_data(char*,int*,float*,int,int,int,float)
+void sort_data(int*,float*,int)
+
+int main()
+
+int load_data(char*,int*,float*,int*)
+void print_data(int*,float *, int)
+int highest_amount(float *, int)
+int lowest_amount(float *,int)
+int average_amount(float *, int)
+int update_data(char*,int*,float*,int)
+void write_data(char*,int*,float*,int,int,int,float)
+void sort_data(int*,float*,int)
+
+
