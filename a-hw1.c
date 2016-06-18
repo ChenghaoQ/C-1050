8125b40c2df2166bbd81a39b912f688ed600ef2c
Zhongyu Li <zli@localhost.localdomain>
2016-06-18 16:40:14 -0500

    unfinished hw1

--- a-hw1.c
+++ a-hw1.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 typedef enum status
 {
 	PLAYING,
@@ -33,5 +34,121 @@ typedef struct board
 }Board;
 
 Board* randomizedBoard(int rows,int columns, int numMines);
+void updateBoard(Board* board, int x, int y);
+int countMines(Board* squares,int x, int y);
+void freeBoard(Board* board);
+
+
+
+int main(int argc,char **argv)
+{
+	srand(time(NULL));////////////////////////
+
+	if(argc!=4)
+	{
+		printf("Correct usage: ./a.out <number of rows><number of columns><number of mines>\n");
+		return -1;
+	}
+
+	Board* b =randomizedBoard(atoi(argv[1]),atoi(argv[2]),atoi(argv[3]));  //Board pointer b 
+
+	while(b->s == PLAYING)
+	{
+		printBoard(b);
+		printf("\nSelect a row: ");
+		int r;
+		scanf("%d",&r);
+		while(r<0||r>b->numRows)
+		{
+			printf("Invalid entry. Please try again:");
+			scanf("%d",&r);
+		}
+		printf("Select a column:");
+		int c;
+		scanf("%d",&c);
+		while(c<0|| c > b->numColumns)
+		{
+			printf("Invalid entry. Try again:");
+			scanf("%d",&c);
+		}
+		
+		if(b->squares[r][c].C==UNCOVERED)
+		{
+			printf("\nThat square is already uncovered!\n");
+		}
+		else
+		{
+			updateBoard(b,r,c);
+		}
+	}
+
+	if(b->s == WON)
+		printf("\nYou won!Congrats!\n");
+	else
+		printf("\nYou hit a mine! Oh no!\n");
+
+	printUncoveredBoard(b);
+
+	freeBoard(b);
+
+
+	return 0;
+}
+
+
+
+
+Board* randomizedBoard(int rows,int columns, int numMines)
+{
+	Board* bor;
+	Square** sqr;
+	int mines=0,r,c,i,j;
+
+	sqr=malloc(sizeof(*sqr)*rows);
+	//initialize the array
+	for(i=0;i<rows;i++)
+	{
+		*(sqr+i)=malloc(sizeof(*(*(sqr+j)))*columns);
+		for(j=0;j<columns;j++)
+		{
+			//set the attribute to each square
+			(*(*(sqr+i)+j)).T=0; //== sqr[i][j].T=NOTAMINE
+			(*(*(sqr+i)+j)).C=0;
+			(*(*(sqr+i)+j)).surroundingMines=-2;
+		}
+
+	}
+	//set up the mines
+	do{
+		r=rand()%rows;
+		c=rand()%columns;
+		if((*(*(sqr+r)+c)).T==0)
+		{
+			(*(*(sqr+r)+c)).T=1;
+			mines++;
+		}
+	}while(mines<numMines);
+	
+	//call countMines to set the surroundingMines value of !each! square
+	//countMines(Board* squares,int x,int y)
+
+
+
+
+
+
+
+}
+
+
+void updateBoard(Board* board, int x, int y);
+int countMines(Board* squares,int x, int y);
+void freeBoard(Board* board);
+
+
+
+
+
+
 
 
