0b9b656ef3b5d924a91efa1e8a2f655c47942e3c
Qian <cqnp3@tc-login.red.lan>
2016-06-21 15:32:04 -0500

    seg seg fault

--- a-hw1.c
+++ a-hw1.c
@@ -37,7 +37,8 @@ Board* randomizedBoard(int rows,int columns, int numMines);
 void updateBoard(Board* board, int x, int y);
 int countMines(Board* squares,int x, int y);
 void freeBoard(Board* board);
-
+void printBoard(Board* board);
+void printUncoveredBoard(Board* board);
 
 
 int main(int argc,char **argv)
@@ -228,8 +229,93 @@ int countMines(Board* squares,int x, int y)
 
 			
 }
-void freeBoard(Board* board);
+void freeBoard(Board* board)
+{
+	int i;
+	for(i=0;i<board->numRows;i++)
+	{
+		free(*(board->squares+i));
+	}
+	free(board->squares);
+	free(board);
+}
 
+
+
+
+
+
+
+
+
+
+
+void printBoard(Board* board)
+{
+	Square** squares = board->squares;
+
+    int i, j;
+    printf("\x1B[0m\n\n");
+    printf("\x1B[0m\t   Columms:\n");
+
+	printf("         ");
+	for(i = 0; i < board->numColumns; i++)
+	{
+		printf("%d ", i);
+	}
+    printf("\n");
+    for(i = 0; i < board->numRows; i++)
+    {
+        printf("\x1B[0m\nRow %d -> ", i);
+        for(j = 0; j < board->numColumns; j++)
+        {
+            if(squares[i][j].C == COVERED)
+            {
+                printf("\x1B[0mx ");
+            }
+            else if (squares[i][j].surroundingMines == 0) 
+            {
+                printf("\x1B[32m%d ", squares[i][j].surroundingMines);
+            } else {
+                printf("\x1B[33m%d ", squares[i][j].surroundingMines);
+            }
+        }
+    }
+	printf("\n\n");
+}
+
+void printUncoveredBoard(Board* board)
+{
+	Square** squares = board->squares;
+
+    int i, j;
+    printf("\x1B[0m\n\n");
+    printf("\x1B[0m\t   Columms:\n");
+    printf("         ");
+	for(i = 0; i < board->numColumns; i++)
+	{
+		printf("%d ", i);
+	}
+    printf("\n");
+    for(i = 0; i < board->numRows; i++)
+    {
+        printf("\x1B[0m\nRow %d -> ", i);
+        for(j = 0; j < board->numColumns; j++)
+        {
+            if(squares[i][j].T == MINE)
+            {
+                printf("\x1B[31mM ");
+            }
+            else if (squares[i][j].surroundingMines == 0) 
+            {
+                printf("\x1B[32m%d ", squares[i][j].surroundingMines);
+            } else {
+                printf("\x1B[33m%d ", squares[i][j].surroundingMines);
+            }
+        }
+    }
+	printf("\n\n");
+}
 
 
 
