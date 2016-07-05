8a8390f12e2dfcbbabf47fdd13cf604f914082f7
Chenghao Qian <Jkob@>
2016-07-04 23:42:32 -0500

    a-prelab5

---
+++ a-prelab5.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct i_
+{
+	int value;
+	struct i_* nextItem;
+}Item;
+//typedef Item *It;
+
+Item* addItem(Item** start, int toAdd);
+void printList(Item* start);
+void freeList(Item* start);
+
+int main()
+{
+	Item* startPtr = NULL;
+	
+	int add;
+	do{
+		printf("Enter a number:");
+		scanf("%d",&add);
+		printf("-----%d------\n",add);
+		addItem(&startPtr,add);
+		printList(startPtr);
+	}while(add!=-1);
+
+	
+	return 0;
+}
+
+
+Item* addItem(Item* *start, int toAdd)
+{
+	Item *new,*previous,*current;//May have problem here
+	new = malloc(sizeof(Item));
+
+	if(new != NULL)
+	{
+		new->value = toAdd;
+		new->nextItem = NULL;
+		previous = NULL;
+		current = *start;
+		
+		while(current != NULL)
+		{
+			previous = current;
+			current=current->nextItem;
+		}
+		
+		if(previous ==NULL)
+		{
+			new->nextItem = *start;
+			*start = new;
+		}
+		else
+		{
+			previous->nextItem = new;
+			new->nextItem = current;
+		}
+	}
+	else
+	{
+		printf("%d not added.No memory available.\n",toAdd);
+	}
+}
+
+
+void printList(Item* start)
+{
+	if(start ==NULL)
+	{
+		printf("List is empty.\n\n");
+	}
+	else
+	{
+		printf("The list is:\n");
+
+		while(start !=NULL)
+		{
+			printf("%d -->",start->value);
+			start = start->nextItem;
+		}
+
+		printf("NULL\n\n");
+	}
+}
+
+
+void freeList(Item* start)
+{
+	Item* temp = NULL;
+	while(start)
+	{
+		temp=start->nextItem;
+		free(start);
+		start=temp;
+	}
+}
+
