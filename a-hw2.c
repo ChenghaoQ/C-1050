c9a938e025258f0c863d8fb231054136aaf53e8c
Chenghao Qian <Jkob@>
2016-07-12 01:13:26 -0500

    hw2

--- a-hw2.c
+++ a-hw2.c
@@ -11,22 +11,100 @@ typedef struct o_
 Order** importLists(char* filename);
 void printList(Order* firstOrder);
 int updateOrder(int ID, Order ** oldList, Order** newList);
-int processOlderstOrder(Order** firstOrder);
+int processOldestOrder(Order** firstOrder);
 void freeList(Order* list);
 int getPriority();
 
 
-int main()
+int main(int argc, char** argv)
 {
+	if(argc != 2)
+	{
+		printf("Usage: ./a.out <filename>\n");
+		return -2;
+	}
 
+	Order** lists = importLists(argv[1]);
+	if(lists == NULL)
+	{
+		printf("Error reading from %s.\n", argv[1]);
+		return -1;
+	}
 
+	while(1)
+	{
+		printf("1: print a list\n");
+		printf("2: change an order's priority\n");
+		printf("3: process oldest order\n");
+		printf("4: quit\n>");
+		int selection;
+		scanf("%d", &selection);
+		while(selection < 1 || selection > 4)
+		{
+			printf("Invalid selection, choose again:\n>");
+			scanf("%d", &selection);
+		}
+	
+		if(selection == 1)
+		{
+			printf("Which list do you want to print out?\n");
+			int priority = getPriority();
+			printList(lists[priority - 1]);
+			
+		}
+		else if(selection == 2)
+		{
+			printf("What is the ID of the order to be changed?\n>");
+			int ID;
+			scanf("%d", &ID);
+			printf("What was the ID's old priority?\n");
+			int priority = getPriority();
+
+			printf("What is the ID's new priority?\n");
+			int newPriority = getPriority();
+
+			int ret = updateOrder(ID, &lists[priority - 1], &lists[newPriority - 1]);		
+			if(ret != ID)
+			{
+				printf("Error updating order no. %d.\n", ID);
+			}
+			else
+			{
+				printf("Order no. %d updated successfully.\n", ID);
+			}
+		}
+		else if(selection == 3)
+		{
+			printf("Which priority list do you want to process from?\n");
+			int priority = getPriority();
+			int ret = processOldestOrder(&lists[priority - 1]);
+			if(ret != 0)
+			{
+				printf("Error processing from list %d. Is it empty?\n", priority);
+			}
+			else
+			{
+				printf("list %d processed successfully.\n", priority);
+			}
+		}
+		else break;
+	}
+
+	int i;
+	for(i = 0; i < 4; i++)
+	{
+		freeList(lists[i]);
+	}
+
+	free(lists);
 	return 0;
 }
+
 Order** importLists(char* filename)
 {
 	
 	//orders=malloc(sizeof(*order)*4);
-	Order *selection=NULL;//*nextday=NULL,*business=NULL,*standard=NULL,*longterm=NULL,*selection=NULL;
+	Order **selection=NULL;//*nextday=NULL,*business=NULL,*standard=NULL,*longterm=NULL,*selection=NULL;
 	Order** orders;
 	orders=malloc(sizeof(Order*)*4);
 
@@ -50,27 +128,28 @@ Order** importLists(char* filename)
 		{
 			switch(z1){
 				case 1:
-					selection=*(orders+0);
+					selection=&*(orders+0);
 					break;
 				case 2:
-					selection=*(orders+1);
+					selection=&*(orders+1);
 					break;
 				case 3:
-					selection=*(orders+2);
+					selection=&*(orders+2);
 					break;
 				case 4:
-					selection=*(orders+3);
+					selection=&*(orders+3);
 					break;
 			}
 
 			Order *new;
 			new = malloc(sizeof(Order));
+			printf("New orders\n");
 			if(new!=NULL)
 			{
 				new->ID=x1;
 				new->daysInQueue=y1;
 				previous = NULL;
-				current = selection;//set current = head
+				current = *selection;//set current = head
 
 				while(current!=NULL)
 				{
@@ -79,13 +158,13 @@ Order** importLists(char* filename)
 				}
 				if(previous ==NULL)
 				{
-					new->nextOrder = selection;
-					selection=new;
+					new->nextOrder =* selection;
+					*selection=new;
 				}
 				else
 				{
 					previous->nextOrder = new;
-					new->nextOrder=current;
+					new->nextOrder=NULL;/////
 				}
 			}
 			else
@@ -105,22 +184,112 @@ Order** importLists(char* filename)
 }
 void printList(Order* firstOrder)
 {
-
-
+	if(firstOrder==NULL)
+	{
+		printf("Currently there is no orders\n");
+	}
+	else
+	{
+		while(firstOrder !=NULL)
+		{
+			printf("Order No. %d: %d days in queue.\n",firstOrder->ID,firstOrder->daysInQueue);
+			firstOrder = firstOrder->nextOrder;
+		}
+		printf("\n\n");
+	}
 }
+
+
 int updateOrder(int ID, Order ** oldList, Order** newList)
 {
+	Order *previous,*current,*temp=NULL;
+	current=*oldList;
+	if(current->ID==ID)
+	{
+		temp=current;
+		current=current->nextOrder;
+		*oldList=current;
+	}
+	while(current!=NULL)
+	{
+		previous=current;
+		if(current->ID==ID)
+		{
+			temp = current;
+			current=current->nextOrder;
+			previous->nextOrder=current;
+			break;
+		}
+		current=current->nextOrder;
+	}
+	if(temp == NULL)
+	{
+		printf("Sorry we cannot find that ID\n");
+		if(ID==0)
+			return 0;
+		else
+			return 1;
+	}
+	
 
 
+
+	//Add to newlist
+	previous=NULL;
+	current=*newList;
+	while(current !=NULL)
+	{
+		previous =current;
+		current = current->nextOrder;
+	}
+	if(previous == NULL)
+	{
+		temp->nextOrder = *newList;
+		*newList = temp;
+	}
+	else
+	{
+		previous->nextOrder = temp;
+		temp->nextOrder = NULL;
+	}
+	return temp->ID;
 }
-int processOlderstOrder(Order** firstOrder)
+int processOldestOrder(Order** firstOrder)
 {
-
-
+	int oldest;
+	Order *current,*previous=NULL,*temp,*temprev=NULL;
+	current=*firstOrder;
+	oldest=current->daysInQueue;
+	while(current)
+	{
+		previous=current;
+		if(current->daysInQueue>oldest)
+		{
+			temprev=previous;
+			temp=current;
+		}
+		current=current->nextOrder;
+	}
+	if(temprev==NULL)
+	{
+		printf("-------------------");
+	}
+	else
+	{
+		temprev->nextOrder=temp->nextOrder;
+		free(temp);
+	}
+	return 0;
 }
 void freeList(Order* list)
 {
-
+	Order * temp =NULL;
+	while(list)
+	{
+		temp = list->nextOrder;
+		free(list);
+		list=temp;
+	}
 
 }
 int getPriority()
